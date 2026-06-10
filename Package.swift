a54d209af718ffd20a1ded7335e705ed62ac1e51
Yansheng Ao <yansheng.ao@citcon.cn>
2026-06-10 17:33:50 +0800

    release: v2.7.0

--- Package.swift
+++ Package.swift
@@ -22,46 +22,14 @@ let package = Package(
         .package(url: "https://github.com/klarna/klarna-mobile-sdk-ios.git", from: "2.10.0"),
     ],
     targets: [
-        .binaryTarget(
-            name: "CPaySDK",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/CPaySDK.xcframework.zip",
-            checksum: "e0b34137d1d5f63fbedd12c7d6c404228ab58a573c64fe46226bbe45f143d158"
-        ),
-        .binaryTarget(
-            name: "CardinalMobile",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/CardinalMobile.xcframework.zip",
-            checksum: "4b22271420ae6f61a1cc6044c02ee955e93fbe69d284465cb8935ee25196877e"
-        ),
-        .binaryTarget(
-            name: "PPRiskMagnes",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/PPRiskMagnes.xcframework.zip",
-            checksum: "f90cceba506180ef60e899773f1e54c53896a52cce74b75766f24e81a97db16a"
-        ),
-        .binaryTarget(
-            name: "CorePayments",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/CorePayments.xcframework.zip",
-            checksum: "ea1ebbb00807dc07ed36dab24d18b04a0026ae511a0b39f5eaa899aeea4bf2c2"
-        ),
-        .binaryTarget(
-            name: "PayPalWebPayments",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/PayPalWebPayments.xcframework.zip",
-            checksum: "1b6749ee2da04139c24b32ba88da73d82075d216ceac3131a83bba4c86439684"
-        ),
-        .binaryTarget(
-            name: "FraudProtection",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/FraudProtection.xcframework.zip",
-            checksum: "c93c6b82121eafda32111a453e7df82ac457c6debc9c6e04a25ec095ec6682c2"
-        ),
-        .binaryTarget(
-            name: "PaymentButtons",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/PaymentButtons.xcframework.zip",
-            checksum: "e53ead3745207c30c7b09ada0b1565693bd0d58f4044839b58e73a23328dbe46"
-        ),
-        .binaryTarget(
-            name: "KlarnaExt",
-            url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/KlarnaExt.xcframework.zip",
-            checksum: "71e0870b03c11a159d7a61e8017687519493e466c66276ef48302fa96ab1abb6"
-        ),
+        .binaryTarget(name: "CPaySDK",           url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/CPaySDK.xcframework.zip",           checksum: "197aaf4bf68a79543b4147c1212f128357052e2890e78b501c7cc01630316e94"),
+        .binaryTarget(name: "CardinalMobile",    url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/CardinalMobile.xcframework.zip",    checksum: "e94133551ef3a3bea6c9214984bd9c81eabc3054d1c7a2ea5857e5eb31a6c0a6"),
+        .binaryTarget(name: "PPRiskMagnes",      url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/PPRiskMagnes.xcframework.zip",      checksum: "4c70126ee3a77808a386439a85bea0a29862ef39fdd2bce733c1f408e4db3dba"),
+        .binaryTarget(name: "CorePayments",      url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/CorePayments.xcframework.zip",      checksum: "bdf6843fe33051144f949b34628bee8969b754bdfeef2c3075bac25cd577e2d6"),
+        .binaryTarget(name: "PayPalWebPayments", url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/PayPalWebPayments.xcframework.zip", checksum: "648ec7e5dc0d11addd61fce14f4781e20d6bb38b30967fc2069da8acc067ddb6"),
+        .binaryTarget(name: "FraudProtection",   url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/FraudProtection.xcframework.zip",   checksum: "33237e08bcf2d2de47e7d270f687f86263b0206ab45992805c2b1e639c9a8387"),
+        .binaryTarget(name: "PaymentButtons",    url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/PaymentButtons.xcframework.zip",    checksum: "de9f250be660f64a22fc440bef12e8fff178a46e4b17a51b6796a38fe879c39f"),
+        .binaryTarget(name: "KlarnaExt",         url: "https://github.com/Citcon/citcon_upi_sdk_ios/releases/download/v2.7.0/KlarnaExt.xcframework.zip",         checksum: "ce901ff6c8a53ecfc3fc9ee60c22d72740a34a285f5b51bf3fa581ef59c4f52a"),
         .target(
             name: "KlarnaExtWrapper",
             dependencies: [
